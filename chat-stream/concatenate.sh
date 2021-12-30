0241604ecafd463ea7187e926537075fda860c30
Paolo Spadoni <paolo_spadoni@yahoo.it>
2021-12-30 09:58:24 +0100

    element

---
+++ chat-stream/concatenate.sh
@@ -0,0 +1,2 @@
+
+ cat dist/chat-stream/runtime.js dist/chat-stream/polyfills.js dist/chat-stream/main.js > chatapp.js 
--- chat-stream/src/app/app.module.ts
+++ chat-stream/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
+import  { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 import { StreamChatModule, StreamAutocompleteTextareaModule } from 'stream-chat-angular';
@@ -9,6 +10,15 @@ import { StreamChatModule, StreamAutocompleteTextareaModule } from 'stream-chat-
   declarations: [AppComponent],
   imports: [BrowserModule, TranslateModule.forRoot(), StreamAutocompleteTextareaModule, StreamChatModule],
   providers: [],
-  bootstrap: [AppComponent],
+  //bootstrap: [AppComponent],
+  entryComponents :  [
+    AppComponent
+ ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector : Injector){}
+  ngDoBootstrap(){
+      const el = createCustomElement(AppComponent, {injector : this.injector});
+      customElements.define('chat-component',el);
+      }
+  }
