469d48b698cf1d0343642ed2d45b4b1e317739c2
Paolo Spadoni <paolo_spadoni@yahoo.it>
2021-12-30 09:11:04 +0100

    chat-stream

---
+++ chat-stream/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatClientService, ChannelService, StreamI18nService } from 'stream-chat-angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'delicate-tree-7';
+    const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGVsaWNhdGUtdHJlZS03IiwiZXhwIjoxNjQwODY4NjEwfQ.Fyf6cN0z_qnjmqDcyXIEWUwSAUlfvWf2yvgZZGLjqyE';
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
+}
