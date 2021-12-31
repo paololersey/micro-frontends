e6812cc4ad71bfb3e15a2436c6082498750e0e7f
Paolo Spadoni <paolo_spadoni@yahoo.it>
2021-12-31 12:32:21 +0100

    nx micro fe

---
+++ ng-mfe/apps/dashboard/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'login',
+          loadChildren: () =>
+            import('login/Module').then((m) => m.RemoteEntryModule),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
